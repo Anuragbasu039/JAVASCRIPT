44ea918f7996438190b20e9849b969613c1e4b0c
anuragbasu039 <anuragbasu039@gmail.com>
2024-09-17 09:28:54 +0530

    mapfilterreduce

---
+++ mapfilterreduce.js
@@ -0,0 +1,34 @@
+//map, filter, reduce
+//What is map?
+// const nums = [1,2,3,4]
+// const multiplythree = nums.map((num, i, arr) => {
+//     return num * 3;
+// })
+// console.log(multiplythree);
+
+//output => [3, 6, 9, 12]
+
+
+
+
+//what is filter?
+// const nums = [1,2,3,4]
+// const morethantwo = nums.filter((num) => {
+//     return num >2
+// })
+// console.log(morethantwo);
+
+//output => [3,4]
+
+
+//what is reduce?
+// const nums = [1,2,3,4]
+
+// const sum = nums.reduce((acc, curr, i, arr) => {
+//     return acc + curr;
+// }, 0)
+// console.log(sum);
+
+//output => 10
+
+
